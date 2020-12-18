256b2a9d690fcd5daa6ac94e70351403e6b118a6
Marcos Rossetto <marcosrst23@gmail.com>
2020-12-18 18:49:26 -0300

    static boot added

--- app/Models/OrderItem.js
+++ app/Models/OrderItem.js
@@ -2,8 +2,13 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
-
 class OrderItem extends Model {
+  static boot() {
+    super.boot()
+
+    this.addHook('beforeSave', 'OrderItemHook.updateSubtotal')
+  }
+
   static get traits() {
     return ['App/Models/Traits/NoTimestamp']
   }
