23ea63a3f0d5898636321be580e917a36da21cdf
Marcos Rossetto <marcosrst23@gmail.com>
2020-12-21 11:15:00 -0300

    category controller update, show and destroy

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -36,7 +36,17 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store({ request, response }) {}
+  async store({ request, response }) {
+    try {
+      const { title, description, image_id } = request.all()
+      const category = await Category.create({ title, description, image_id })
+      return response.status(201).send(category)
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Erro ao processar a sua solicitação.',
+      })
+    }
+  }
 
   /**
    * Display a single category.
@@ -47,7 +57,10 @@ class CategoryController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show({ params, request, response, view }) {}
+  async show({ params: { id }, request, response, view }) {
+    const category = await Category.findOrFail(id)
+    return response.send(category)
+  }
 
   /**
    * Update category details.
@@ -57,7 +70,13 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update({ params, request, response }) {}
+  async update({ params: { id }, request, response }) {
+    const category = await Category.findOrFail(id)
+    const { title, description, image_id } = request.all()
+    category.merge({ title, description, image_id })
+    await category.save()
+    return response.send(category)
+  }
 
   /**
    * Delete a category with id.
@@ -67,7 +86,11 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy({ params, request, response }) {}
+  async destroy({ params: { id }, request, response }) {
+    const category = await Category.findOrFail(id)
+    await category.delete()
+    return response.status(204).send()
+  }
 }
 
 module.exports = CategoryController
