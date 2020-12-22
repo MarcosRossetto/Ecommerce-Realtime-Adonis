73173434bd4cc058ffbee9c11dd7ae814bf26782
Marcos Rossetto <marcosrst23@gmail.com>
2020-12-22 10:36:44 -0300

    controller index and delete order

--- app/Controllers/Http/Admin/OrderController.js
+++ app/Controllers/Http/Admin/OrderController.js
@@ -3,6 +3,8 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
+const Order = use('App/Models/Order')
+const Database = use('Database')
 
 /**
  * Resourceful controller for interacting with orders
@@ -17,7 +19,22 @@ class OrderController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index({ request, response, view }) {}
+  async index({ request, response, pagination }) {
+    const { status, id } = request.only(['status', 'id'])
+    const query = Order.query()
+
+    if (status && id) {
+      query.where('status', status)
+      query.orWhere('id', 'LIKE', `%${id}%`)
+    } else if (status) {
+      query.where('status', status)
+    } else if (id) {
+      query.where('id', 'LIKE', `%${id}%`)
+    }
+
+    const orders = query.paginate(pagination.pages, pagination.limit)
+    return response.send(orders)
+  }
 
   /**
    * Create/save a new order.
@@ -38,7 +55,10 @@ class OrderController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show({ params, request, response, view }) {}
+  async show({ params: { id }, request, response, view }) {
+    const order = await Order.findOrFail(id)
+    return response.send(order)
+  }
 
   /**
    * Update order details.
@@ -58,7 +78,25 @@ class OrderController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy({ params, request, response }) {}
+  async destroy({ params: { id }, request, response }) {
+    const order = await Order.findOrFail(id)
+    const trx = await Database.beginTransaction()
+
+    try {
+      await order.items().delete(trx)
+      await order.coupons().delete(trx)
+      await order.delete(trx)
+      await trx.commit()
+
+      return response.status(204).send()
+    } catch (error) {
+      await trx.rollback()
+
+      return response.status(400).send({
+        message: 'Não foi possível deletar o pedido.',
+      })
+    }
+  }
 }
 
 module.exports = OrderController
